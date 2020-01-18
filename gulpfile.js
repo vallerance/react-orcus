157b7b5fee63fbb288f9f166afc82c05e6decc5c
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2020-01-18 14:15:03 -0800

    Create gulpfile.js for running dev tasks

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+/* gulpfile.js
+ * Glup task-runner configruation for project
+ * Dependencies: dev-tasks, gulp, gulp-util modules
+ * Author: Joshua Carter
+ * Created: June 08, 2019
+ */
+"use strict";
+//include modules
+var DevOps = require("dev-tasks"),
+    gulp = require("gulp"),
+    gutil = require("gulp-util");
+
+//configure dev-tasks
+DevOps.init({
+    appName: 'react-orcus',
+    bundleDir: "dist",
+    bundleName: "react-orcus",
+    wpSingleEntryPoint: "./src/index.js",
+    wpExtOptions: {
+        output: {
+            library: "Orcus",
+            libraryTarget: "umd"
+        }
+    },
+    gitCommitterName: 'ReactOrcusDevTasks',
+    gitCommitterEmail: 'coosriverjoshua1@outlook.com'
+});
+
+//default gulp task: documentation
+gulp.task('default', function () {
+    gutil.log(
+`
+
+Available Gulp Commands:
+ - lint
+ - build
+ - bundle
+ - minify
+ - release major|minor|patch
+`
+    );
+});
+
+//lint code using ESLint
+gulp.task('lint', function () {
+    return DevOps.lint();
+});
+
+//transpile code using babel
+gulp.task('build', function () {
+    //lint first
+    DevOps.lint();
+    return DevOps.build();
+});
+
+//build code using webpack and babel
+gulp.task('bundle', function () {
+    //lint first
+    DevOps.lint();
+    return DevOps.bundle();
+});
+
+//build our code and minify it using webpack and babili
+gulp.task('minify', function () {
+    //lint first
+    DevOps.lint();
+    //run build again
+    return DevOps.bundle().then(function () {
+        //now minify
+        return DevOps.bundle("production", true);
+    });
+});
+
+//create a new release and push it to master
+gulp.task('release', function () {
+    return DevOps.release();
+});
+
+
+//create dummy tasks so that we can use non-hyphentated arguments
+var dummy = function () {
+        return;
+    },
+    dummies = ['patch', 'minor', 'major'];
+for (let i=0; i<dummies.length; i++) {
+    gulp.task(dummies[i], dummy);
+}
