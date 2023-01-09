1a4c872828cdb1dab52a0f066e49f073893cdaa4
Joshua Carter <carter.joshua.603@gmail.com>
2023-01-09 09:04:20 -0800

    Manually send statuses to GitHub

---
+++ codefresh/status.Dockerfile
@@ -0,0 +1,16 @@
+FROM node:18-alpine
+
+RUN apk add bash curl
+
+RUN npm install axios
+
+COPY status.js .
+
+ENTRYPOINT node status.js
+
+ARG GITHUB_TOKEN
+ENV GITHUB_TOKEN=${GITHUB_TOKEN}
+ARG CF_REVISION
+ENV CF_REVISION=${CF_REVISION}
+ARG CF_BUILD_URL
+ENV CF_BUILD_URL=${CF_BUILD_URL}
---
+++ codefresh/status.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+const { GITHUB_TOKEN, CF_BUILD_URL, CF_REVISION } = process.env;
+
+const [stage, state] = process.argv.slice(2);
+
+const descriptions = {
+    pending: 'Build pending',
+    success: 'Build successful',
+    failure: 'Build failed',
+};
+const description = descriptions[state];
+
+axios.post(
+    `https://api.github.com/repos/vallerance/react-orcus/statuses/${CF_REVISION}`,
+    {
+        state,
+        target_url: CF_BUILD_URL,
+        description,
+        context: `react-orcus/push/${stage}`,
+    },
+    {
+        headers: {
+            Accept: 'application/vnd.github+json',
+            Authorization: `Bearer ${GITHUB_TOKEN}`,
+            'X-GitHub-Api-Version': '2022-11-28',
+        },
+    }
+);
