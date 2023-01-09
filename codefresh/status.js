bb10e4ffe853ef2f790807008843b8ae829f2632
Joshua Carter <carter.joshua.603@gmail.com>
2023-01-09 09:26:25 -0800

    Log axios errors to console

--- codefresh/status.js
+++ codefresh/status.js
@@ -11,19 +11,21 @@ const descriptions = {
 };
 const description = descriptions[state];
 
-axios.post(
-    `https://api.github.com/repos/vallerance/react-orcus/statuses/${CF_REVISION}`,
-    {
-        state,
-        target_url: CF_BUILD_URL,
-        description,
-        context: `react-orcus/push/${stage}`,
-    },
-    {
-        headers: {
-            Accept: 'application/vnd.github+json',
-            Authorization: `Bearer ${GITHUB_TOKEN}`,
-            'X-GitHub-Api-Version': '2022-11-28',
+axios
+    .post(
+        `https://api.github.com/repos/vallerance/react-orcus/statuses/${CF_REVISION}`,
+        {
+            state,
+            target_url: CF_BUILD_URL,
+            description,
+            context: `react-orcus/push/${stage}`,
         },
-    }
-);
+        {
+            headers: {
+                Accept: 'application/vnd.github+json',
+                Authorization: `Bearer ${GITHUB_TOKEN}`,
+                'X-GitHub-Api-Version': '2022-11-28',
+            },
+        }
+    )
+    .catch(error => console.error(error));
