959b9d7950c8993de4974212d4226621f2c6b5a4
Joshua Carter <carter.joshua.603@gmail.com>
2023-01-10 11:08:32 -0800

    Install webpack visualizer plugin

--- packages/react-orcus/webpack.config.js
+++ packages/react-orcus/webpack.config.js
@@ -1,5 +1,7 @@
 // require the main @nrwl/react/plugins/webpack configuration function.
 const nrwlConfig = require('@nrwl/react/plugins/webpack');
+const path = require('node:path');
+const Visualizer = require('webpack-visualizer-plugin2');
 
 module.exports = (config, _context) => {
     // first call it so that it @nrwl/react plugin adds its configs,
@@ -29,6 +31,25 @@ module.exports = (config, _context) => {
             library: 'Orcus',
             libraryTarget: 'umd',
         },
-        plugins: [...config.plugins],
+        plugins: [
+            ...config.plugins,
+            ...(process.env.ENABLE_STATS
+                ? [
+                      new Visualizer({
+                          filename: 'statistics.html',
+                      }),
+                  ]
+                : []),
+        ],
+        // resolve: {
+        //     ...config.resolve,
+        //     alias: {
+        //         ...config.resolve.alias,
+        //         'redux-orm': path.resolve(
+        //             __dirname,
+        //             '../../node_modules/redux-orm/src'
+        //         ),
+        //     },
+        // },
     };
 };
